569358766fedb8ab548715962e5dd19687da963d
Manikanta-Hanmakonda <manikanta030305@gmail.com>
2024-09-29 18:11:04 +0530

    Add files via upload

---
+++ chatserver.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+
+public class ChatServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Client: " + inputLine);
+            out.println(inputLine);
+        }
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChatServer server = new ChatServer();
+        server.start(6666);
+    }
+}
